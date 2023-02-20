9ffb5ad00772aac894c04820f9956a201963e068
Soudharm Patil <soupatil@deloitte.com>
2023-02-20 13:03:34 +0530

    20feb

--- Data_Quality_Framework/Data_Quality_Framework/settings.py
+++ Data_Quality_Framework/Data_Quality_Framework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6z*jrj6^&x60sn8gr!@%77dabs$9=w$&1t69tzs#k6so#e$312
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
